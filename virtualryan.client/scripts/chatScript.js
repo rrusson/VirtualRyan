95be36ef4b61ae6124fa30472860b9e6086cea9d
Ryan Russon <it@rrusson.com>
2025-07-18 22:06:51 -0400

    Clean-up

--- virtualryan.client/scripts/chatScript.js
+++ virtualryan.client/scripts/chatScript.js
@@ -5,7 +5,7 @@ var $messages = $('.messages-content'),
 $(window).load(function () {
 	$messages.mCustomScrollbar();
 	setTimeout(function () {
-		addBotResponse("Hi! I'm a bot that answers questions about Ryan's resume and qualifications. What would you like to know?");
+		addBotResponse("Hi! I'm a bot that answers questions about Ryan Russon's resume and qualifications. What would you like to know?");
 		$("#chatQuestion").focus();
 	}, 100);
 });
@@ -86,36 +86,3 @@ $(window).on('keydown', function (e) {
 		return false;
 	}
 })
-
-var Fake = [
-	'Hi there, I\'m Fabio and you?',
-	'Nice to meet you',
-	'How are you?',
-	'Not too bad, thanks',
-	'What do you do?',
-	'That\'s awesome',
-	'Codepen is a nice place to stay',
-	'I think you\'re a nice person',
-	'Why do you think that?',
-	'Can you explain?',
-	'Anyway I\'ve gotta go now',
-	'It was a pleasure chat with you',
-	'Time to make a new codepen',
-	'Bye',
-	':)'
-]
-
-function fakeMessage() {
-	if ($('.message-input').val() != '') {
-		return false;
-	}
-	showBotLoading();
-
-	setTimeout(function () {
-		hideBotLoading();
-		$('<div class="message new"><figure class="avatar"><img src="https://avatars.githubusercontent.com/u/653188?v=4" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
-		setDate();
-		updateScrollbar();
-		i++;
-	}, 1000 + (Math.random() * 20) * 100);
-}
